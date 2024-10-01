6fa5ab92aa0a643290300ebbaa5e7da57cc1342b
angelolaera <angelo.laera.1995@gmail.com>
2024-10-01 15:35:27 +0200

    Inizializzazione + header + navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import BarraNavigazione from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <BarraNavigazione />
+    </>
+  );
+}
+
+export default App;
